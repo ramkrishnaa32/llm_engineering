a3cd4b7730590135dad8b5159500fb621c01cda1
K Ramkrishna Achary <kramkrishnaachary@Rams-MacBook-Pro.local>
2025-04-30 22:36:01 +0530

    updated few scripts

--- openai/summarize.py
+++ openai/summarize.py
@@ -1,21 +1,26 @@
 # imports
-
 import os
 import requests
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from IPython.display import Markdown, display
 from openai import OpenAI
-import openai
 
-my_api_key = os.getenv("OPENAI_API_KEY")
-openai.organization = "org-kIXSuk9NH3eKkkCNipsd2NYQ"
+load_dotenv()
 
-print(f"Using API key --> {my_api_key} <--")
+api_key = os.getenv("OPENAI_API_KEY")
+project_id = os.getenv("OPENAI_PROJECT_ID")
+organization_id = os.getenv("OPENAI_ORG_ID")
 
-openai = OpenAI()
-completion = openai.chat.completions.create(
-    model='gpt-4.1',
-    messages=[{"role":"user", "content": "What's 2+2?"}],
+client = OpenAI(
+    api_key = api_key,
+    project=project_id,
+    organization=organization_id
 )
-print(completion.choices[0].message.content)
+
+response = client.chat.completions.create(
+    model="gpt-4",
+    messages=[{"role": "user", "content": "What's 2+2?"}]
+)
+
+print(response.choices[0].message.content)
