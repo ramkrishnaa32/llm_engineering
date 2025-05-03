98be0574ccb0e8636f19b812db67d06d15516b59
K Ramkrishna Achary <kramkrishnaachary@Rams-MacBook-Pro.local>
2025-05-03 11:34:57 +0530

    updated the file

--- openai/summarize.py
+++ openai/summarize.py
@@ -5,12 +5,22 @@ from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from IPython.display import Markdown, display
 from openai import OpenAI
+import openai
 
-load_dotenv()
+load_dotenv(override=True)
 
 api_key = os.getenv("OPENAI_API_KEY")
 project_id = os.getenv("OPENAI_PROJECT_ID")
 organization_id = os.getenv("OPENAI_ORG_ID")
+
+if not api_key:
+    print("No API key was found - identify & fix!")
+elif not api_key.startswith("sk-proj-"):
+    print("An API key was found, but it doesn't start sk-proj-")
+elif api_key.strip() != api_key:
+    print("An API key was found, but it looks like it might have space or tab characters at the start or end")
+else:
+    print("API key found and looks good so far!")
 
 client = OpenAI(
     api_key = api_key,
@@ -18,9 +28,74 @@ client = OpenAI(
     organization=organization_id
 )
 
-response = client.chat.completions.create(
-    model="gpt-4",
-    messages=[{"role": "user", "content": "What's 2+2?"}]
-)
-
+message = "Hello, GPT! This is my first ever message to you! Hi!"
+response = openai.chat.completions.create(model="gpt-4o-mini", messages=[{"role":"user", "content":message}])
 print(response.choices[0].message.content)
+
+# A class to represent a Webpage
+# Some websites need you to use proper headers when fetching them:
+headers = {
+ "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+class Website:
+
+    def __init__(self, url):
+        """
+        Create this Website object from the given url using the BeautifulSoup library
+        """
+        self.url = url
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.title = soup.title.string if soup.title else "No title found"
+        for irrelevant in soup.body(["script", "style", "img", "input"]):
+            irrelevant.decompose()
+        self.text = soup.body.get_text(separator="\n", strip=True)
+
+ed = Website("https://google.com/")
+
+# print(ed.title)
+# print(ed.text)
+
+# system prompt
+system_prompt = "You are an assistant that analyzes the contents of a website \
+and provides a short summary, ignoring text that might be navigation related. \
+Respond in markdown."
+
+def user_prompt_for(website):
+    user_prompt = f"You are looking at a website titled {website.title}"
+    user_prompt += "\nThe contents of this website is as follows; \
+                    please provide a short summary of this website in markdown. \
+                    If it includes news or announcements, then summarize these too.\n\n"
+    
+    user_prompt += website.text
+    return user_prompt
+
+# print(user_prompt_for(ed))
+
+messages = [
+    {"role": "system", "content": "You are a snarky assistant"},
+    {"role": "user", "content": "What is 2 + 2?"}
+]
+
+response = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
+print(response.choices[0].message.content)
+
+def messages_for(website):
+    return [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_for(website)}
+    ]
+
+def summarize(url):
+    website = Website(url)
+    response = openai.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages_for(website)
+    )
+    return response.choices[0].message.content
+
+def display_summary(url):
+    summary = summarize(url)
+    display(Markdown(summary))
+
+display_summary("https://edwarddonner.com")
